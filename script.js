c6e3a37c8f241c87a2fcebb59c0a2ff87c706103
Vanessa Decopin <vanedecopin@gmail.com>
2024-03-09 21:33:01 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict"
+
+let inputEl = document.querySelector(".input-containter");
+let btn = document.querySelector(".clearBtn");
+
+
+
+
+
+
+let addedItem ((a)=>{
+
+})
+
+
+let addToDo ((b)=>{
+
+})
+
+
+
+
+
+btn.addEventListener("click" addedItem);
+inputEl.addEventListener("submit" addToDo);
